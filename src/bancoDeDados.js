4a347c507efaead28f4639e57cbca4b0a55b90b6
geisa <geisa.pinto@pmweb.com.br>
2023-10-13 14:40:23 -0300

    att banco de dados

--- src/bancoDeDados.js
+++ src/bancoDeDados.js
@@ -1,12 +1,18 @@
 let alunos = [
   {
     id: 1,
-    nome: "Jessica",
-    email: "jessica@teste.com",
-    senha: "123",
+    nome: "Ge",
+    email: "gespa@teste.com",
+    senha: "9936835",
   },
   {
     id: 2,
+    nome: "Caio",
+    email: "caio@teste.com",
+    senha: "123",
+  },
+  {
+    id: 3,
     nome: "Bia",
     email: "bia@teste.com",
     senha: "4321",
